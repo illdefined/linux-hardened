61872ec1403418947b5c6cc3b10142b781b261b0
Mikael Voss <mvs@nyantec.com>
2025-06-03 13:58:31 +0200

    Disable ca-derivations experimental feature

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   };
 
   nixConfig = {
-    extra-experimental-features = [ "pipe-operator" "ca-derivations" ];
+    extra-experimental-features = [ "pipe-operator" ];
     extra-substituters = [ "https://cache.kyouma.net" ];
     extra-trusted-public-keys = [ "cache.kyouma.net:Frjwu4q1rnwE/MnSTmX9yx86GNA/z3p/oElGvucLiZg=" ];
   };
