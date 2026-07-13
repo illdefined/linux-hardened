07400bc339a43dca8736b4b71942dea995c4dd72
Mikael Voss <mvs@nyantec.com>
2026-07-13 14:35:09 +0200

    Update 7.1.2 → 7.1.3

--- package.nix
+++ package.nix
@@ -80,7 +80,7 @@ in stdenv.mkDerivation (finalAttrs: {
   __structuredAttrs = true;
 
   pname = "linux-hardened";
-  version = "7.1.2-hardened1";
+  version = "7.1.3-hardened1";
 
   modDirVersion = lib.versions.pad 3 finalAttrs.version;
 
@@ -88,7 +88,7 @@ in stdenv.mkDerivation (finalAttrs: {
     owner = "anthraxx";
     repo = "linux-hardened";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-rhdOQG3Iq7JBU7KznyqVCQ65se9bKTqcj2tR8stqfw0=";
+    hash = "sha256-zZvm4z0KIvOOukSuSuBixBBItKiGAMbSQbezCCMEarg=";
   };
 
   strictDeps = true;
